b8164b4675dc1fd503d9cbdb48c8bb6cca0f3b4f
nkartikaxioned <kartikn@axioned.com>
2023-12-01 17:59:44 +0530

    functionality add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
-/* Author: 
+const glass = document.querySelectorAll('.glass'),
+emptyContainer = document.querySelector('.empty-container'),
+filledContainer = document.querySelector('.filled-container'),
+precent = document.querySelector('.precent'),
+incrementalValue = 40.3,
+percentValue = 12.5;
 
-*/
+window.addEventListener('load', () => {
+
+
+    glass.forEach((elem,index) => {
+        elem.addEventListener('click', () => {
+            removeActive();
+            for(let i = 0 ; i <= index ; i++) {
+                   glass[i].classList.add('glass-active');
+            }
+            let number = index+1;
+            let finalValue = 322-(Math.floor(number * incrementalValue));
+            emptyContainer.style.height = `${finalValue}px`;
+            let percentage = Math.round(number*percentValue);
+            precent.innerHTML=`${percentage}%`
+        })
+    })
+
+    function removeActive() {
+        glass.forEach((ele) => {
+            ele.classList.remove('glass-active');
+        })
+    }
+
+});
 
 
 
